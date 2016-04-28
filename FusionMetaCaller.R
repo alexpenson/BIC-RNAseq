8be7e77f8190a2510c8204bde37e03436929841a
Alexander Penson <pensona@cbio.mskcc.org>
2016-04-28 11:22:41 -0400

    cope with cases with a single fusion called

--- FusionMetaCaller.R
+++ FusionMetaCaller.R
@@ -2,6 +2,82 @@
 library(data.table)
 library(FusionMetaCaller)
 
+FusionMetaCaller <- function (countMatrix, vote = 2, plot = F, trueFusion = NA){
+  filterInd <- apply(countMatrix > 0, 1, sum) >= vote
+  if(sum(filterInd) == 1){
+    filterMatrix <- t(as.matrix(countMatrix[filterInd, ]))
+    rownames(filterMatrix) <- rownames(countMatrix)[filterInd]
+  } else {
+    filterMatrix <- countMatrix[filterInd, ]
+  }
+  orderMatrix <- matrix(0, nrow(filterMatrix), ncol(filterMatrix))
+  for (j in 1:ncol(filterMatrix)) {
+    ind <- filterMatrix[, j] != 0
+    orderMatrix[ind, j] <- order(filterMatrix[ind, j])
+  }
+  rankSum <- apply(orderMatrix, 1, sum)
+  orderOfRankSum <- order(-rankSum)
+  sortMatrix <- filterMatrix[orderOfRankSum, ]
+  if (plot == T) {
+    co <- 2
+    colLabel <- c("red", "blue", "green", "yellow", "purple",
+                  "chocolate1", "hotpink", "orange", "darkblue", "darkgreen")
+    ltyLabel <- c(rep(c(1, 5), each = 10))
+    mark <- rep(0, times = nrow(countMatrix))
+    mark[rownames(countMatrix) %in% trueFusion] <- 1
+    pdf("precision_recall_plot.pdf")
+    par(mar = c(5.1, 4.1, 4.1, 8.1), xpd = TRUE)
+    plot(x = NULL, y = NULL, xlim = c(0, 1), ylim = c(0,
+                                                      1), xlab = "Recall", ylab = "Precision", main = "Precison-Recall Plot",
+         cex.lab = 1.5, cex.axis = 1.3)
+    for (j in 1:ncol(countMatrix)) {
+      sortCount <- sort(countMatrix[, j], decreasing = T)
+      numFusion <- sum(sortCount > 0)
+      sortMark <- mark[order(countMatrix[, j], decreasing = T)]
+      sortMark <- sortMark[1:numFusion]
+      CUTOFF <- seq(from = 1, to = length(sortMark), by = co)
+      if (CUTOFF[length(CUTOFF)] != length(sortMark)) {
+        CUTOFF <- c(CUTOFF, length(sortMark))
+      }
+      precision <- rep(0, times = length(CUTOFF))
+      recall <- rep(0, times = length(CUTOFF))
+      for (i in 1:length(CUTOFF)) {
+        cutoff <- CUTOFF[i]
+        TP <- sum(sortMark[1:cutoff])
+        precision[i] <- TP/cutoff
+        recall[i] <- TP/length(trueFusion)
+      }
+      collabel <- colLabel[j%%10]
+      ltylabel <- ltyLabel[j%%20]
+      lines(recall, precision, col = collabel, lwd = 2,
+            lty = ltylabel)
+    }
+    sortMark <- rownames(sortMatrix) %in% trueFusion
+    CUTOFF <- seq(from = 1, to = length(sortMark), by = co)
+    if (CUTOFF[length(CUTOFF)] != length(sortMark)) {
+      CUTOFF <- c(CUTOFF, length(sortMark))
+    }
+    precision <- rep(0, times = length(CUTOFF))
+    recall <- rep(0, times = length(CUTOFF))
+    for (i in 1:length(CUTOFF)) {
+      cutoff <- CUTOFF[i]
+      TP <- sum(sortMark[1:cutoff])
+      precision[i] <- TP/cutoff
+      recall[i] <- TP/length(trueFusion)
+    }
+    collabel <- "black"
+    ltylabel <- 2
+    lines(recall, precision, col = collabel, lwd = 2, lty = ltylabel)
+    legend("topright", inset = c(-0.35, 0), legend = c(colnames(countMatrix),
+                                                       "meta-caller"), lty = c(rep(ltyLabel, length = ncol(countMatrix)),
+                                                                               2), lwd = 3, col = c(rep(colLabel, length = ncol(countMatrix)),
+                                                                                                    "black"), bty = "n")
+    dev.off()
+  }
+  return(list(sortMatrix = sortMatrix))
+}
+
+
 annotate_merged_file <- function(merged,
                                  TAG_name = "TAG_breakpoints",
                                  FusionMetaCaller_rank_name = "FusionMetaCaller_rank_breakpoints"){
